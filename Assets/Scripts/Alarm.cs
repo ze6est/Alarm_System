c968eb03484f51d407a3e6ca8dd3af9ac2b24934
Roman <ze6est1@yandex.ru>
2022-07-16 15:31:26 +0300

    removed code duplication script Alarm

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Alarm : MonoBehaviour
 {
-    [SerializeField] private float _maxDeltaVolume = 0.001f;    
+    [SerializeField] private float _maxDeltaVolume = 0.001f;
 
-    private AudioSource _audioSource;    
+    private AudioSource _audioSource;
     private float _targetVolume = 1.0f;
+    private float _minVolume = 0f;
+    private int _soundReduction = -1;
     private IEnumerator _currentCoroutine;
 
     private void Awake()
@@ -19,51 +21,55 @@ public class Alarm : MonoBehaviour
     private void Start()
     {
         _audioSource.Play();
-    }    
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<Player>())
+        if (other.GetComponent<Player>())
         {
-            if(_currentCoroutine != null)
-            {
-                StopCoroutine(_currentCoroutine);
-            }
-
-            _currentCoroutine = IncreaseVolume();
-            StartCoroutine(_currentCoroutine);
-        }        
-    }    
+            TurnOnCoroutine(IncreaseVolume());
+        }
+    }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.GetComponent<Player>())
         {
-            if (_currentCoroutine != null)
-            {
-                StopCoroutine(_currentCoroutine);
-            }
-
-            _currentCoroutine = TurnDownVolume();
-            StartCoroutine(_currentCoroutine);
+            TurnOnCoroutine(TurnDownVolume());
         }
     }
 
     private IEnumerator IncreaseVolume()
     {
-        while(_audioSource.volume < _targetVolume)
+        while (_audioSource.volume < _targetVolume)
         {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetVolume, _maxDeltaVolume);
+            ChangeVolume();
             yield return null;
-        }        
+        }
     }
 
     private IEnumerator TurnDownVolume()
     {
-        while(_audioSource.volume > 0)
+        while (_audioSource.volume > _minVolume)
         {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetVolume, -_maxDeltaVolume);
+            ChangeVolume(_soundReduction);
             yield return null;
-        }              
+        }
+    }
+
+    private void TurnOnCoroutine(IEnumerator includedCoroutine)
+    {
+        if (_currentCoroutine != null)
+        {
+            StopCoroutine(_currentCoroutine);
+        }
+
+        _currentCoroutine = includedCoroutine;
+        StartCoroutine(_currentCoroutine);
+    }
+
+    private void ChangeVolume(int changeDirection = 1)
+    {
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetVolume, _maxDeltaVolume * changeDirection);
     }
 }
